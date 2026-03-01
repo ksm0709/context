dc36884dbc66a7695d5c18258daa7b5d91ab7185
TaehoKang <ksm07091@gmail.com>
2026-03-02 00:34:17 +0900

    style: fix prettier formatting on eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -39,7 +39,10 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'warn',
       'no-console': 'error',
       'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
     },
   }
 );
